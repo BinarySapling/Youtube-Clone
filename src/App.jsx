3885a280753661c6ad27d65cea8ef690b96090d5
Danish <danishanwarofficial@gmail.com>
2026-02-17 10:45:22 +0530

    feat: integrate react-router-dom for routing and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import './App.css'
 import SideBar from './components/SideBar'
+import { Route, Routes } from 'react-router-dom'
 const App = () => {
   return (
     <>
-     <NavBar/> 
-     <SideBar/>
+    <Layout>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/watch/:id' element={<Watch/>}/>
+          <Route path='/upload' element={<Upload/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+        </Routes>
+    </Layout>
     </>
   )
 }
