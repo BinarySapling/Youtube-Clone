6674f2edc481c14d7c13287b54b4272b0c8ad800
Danish <danishanwarofficial@gmail.com>
2026-02-17 12:18:12 +0530

    feat: add YouTube API integration to fetch trending videos and update Home component

---
+++ src/api/youtube.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "https://www.googleapis.com/youtube/v3";
+const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
+
+export const fetchTrendingVideos = async () => {
+  const response = await axios.get(`${BASE_URL}/videos`, {
+    params: {
+      part: "snippet,statistics",
+      chart: "mostPopular",
+      regionCode: "IN",
+      maxResults: 12,
+      key: API_KEY,
+    },
+  });
+
+  return response.data.items;
+};
